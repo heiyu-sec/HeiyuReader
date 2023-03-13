08aa1f6bee53f6e4b3987625076c947ce6832d4d
heiyu <304429477@qq.com>
2023-03-13 10:51:35 +0800

    新增service

---
+++ src/main/java/com/imooc/reader/service/TestService.java
@@ -0,0 +1,24 @@
+package com.imooc.reader.service;
+
+import com.imooc.reader.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 描述：TODO
+ */
+
+@Service
+public class TestService {
+    @Resource
+    private TestMapper testMapper;
+    public void batchImport(){
+        for(int i = 0 ; i < 5 ;i++){
+            /*if(i==2){
+                throw new RuntimeException("未处理异常");
+            }*/
+            testMapper.insertSample();
+        }
+    }
+}
