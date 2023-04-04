aeee4e7519995dad7babbcb178e6bf81f21cc044
heiyu <304429477@qq.com>
2023-04-04 10:12:58 +0800

    增加事务到service

--- src/main/java/com/imooc/reader/service/TestService.java
+++ src/main/java/com/imooc/reader/service/TestService.java
@@ -2,6 +2,7 @@ package com.imooc.reader.service;
 
 import com.imooc.reader.mapper.TestMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -11,13 +12,17 @@ import javax.annotation.Resource;
 
 @Service
 public class TestService {
+
+
     @Resource
     private TestMapper testMapper;
+
+    @Transactional
     public void batchImport(){
         for(int i = 0 ; i < 5 ;i++){
-            /*if(i==2){
+            if(i==2){
                 throw new RuntimeException("未处理异常");
-            }*/
+            }
             testMapper.insertSample();
         }
     }
