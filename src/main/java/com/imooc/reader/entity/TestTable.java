1bcef41f5fec3379d7bfe5d84f6abb55a3df28c9
heiyu <304429477@qq.com>
2023-05-15 19:23:27 +0800

    新增实体文件

---
+++ src/main/java/com/imooc/reader/entity/TestTable.java
@@ -0,0 +1,26 @@
+package com.imooc.reader.entity;
+
+/**
+ * 描述：TODO
+ */
+
+public class TestTable {
+    private Integer id;
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
